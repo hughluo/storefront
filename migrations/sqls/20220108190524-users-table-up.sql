2af2e67a3da4c01358ffd4db5204f33adf8eb23d
Yinchi Luo <bugwriter@outlook.com>
2022-01-09 08:54:31 +0100

    make username UNIQUE and password_digest NOT NULL in table users

--- migrations/sqls/20220108190524-users-table-up.sql
+++ migrations/sqls/20220108190524-users-table-up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(100),
-    password_digest VARCHAR
+    username VARCHAR(100) UNIQUE,
+    password_digest VARCHAR NOT NULL
 );
