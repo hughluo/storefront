b308125add6b7c2db7e93859658082d6ed4a59d5
Yinchi Luo <bugwriter@outlook.com>
2022-01-09 12:34:04 +0100

    test and impl product model

---
+++ spec/models/productSpec.ts
@@ -0,0 +1,51 @@
+import dotenv from 'dotenv'
+import { Product, ProductStore } from '../../src/models/product'
+
+const store = new ProductStore
+
+const testProductName = 'MagicCookie'
+const testProductPrice = 20
+
+export const createTestProduct = async (name: string, price: number): Promise<Product> => {
+  const gotProduct = await store.create(name, price)
+  expect(gotProduct.name).toBe(name)
+  expect(gotProduct.price).toBe(price)
+  return gotProduct
+}
+
+export const deleteTestProduct = async (id: string) => {
+  const deletedRowCount = await store.delete(id)
+  expect(deletedRowCount).toBe(1)
+}
+
+describe('Test ProductStore', () => {
+
+  it('create product', async () => {
+    const product = await createTestProduct(testProductName, testProductPrice)
+    await deleteTestProduct(product.id)
+  })
+
+  it('show product', async () => {
+    const product = await createTestProduct(testProductName, testProductPrice)
+    const showedProduct = await store.show(product.id)
+    expect(product).toEqual(showedProduct)
+    await deleteTestProduct(product.id)
+  })
+  
+  it('index product', async () => {
+    const p1Name = 'p1'
+    const p1Price = 10
+    const p2Name = 'p2'
+    const p2Price = 20
+    const p1 = await createTestProduct(p1Name, p1Price)
+    const p2 = await createTestProduct(p2Name, p2Price)
+
+    const products = await store.index()
+    expect(products).toHaveSize(2)
+    expect(products).toContain(p1)
+    expect(products).toContain(p2)
+
+    await deleteTestProduct(p1.id)
+    await deleteTestProduct(p2.id)
+  })
+})
---
+++ src/models/product.ts
@@ -0,0 +1,72 @@
+import client from '../database'
+
+export type Product = {
+  id: string
+  name: string
+  price: number
+}
+
+export class ProductStore {
+  index = async (): Promise<Array<Product>> => {
+    try {
+      const conn = await client.connect()
+      const sql = 'SELECT * FROM products'
+
+      const result = await conn.query(sql)
+      const products = result.rows
+
+      conn.release()
+      return products
+    } catch (err) {
+      throw new Error(`unable index products: ${err}`)
+    }
+  }
+
+  show = async (id: string): Promise<Product> => {
+    try {
+      const conn = await client.connect()
+      const sql = 'SELECT * FROM products WHERE id=($1)'
+
+      const result = await conn.query(sql, [id])
+      const product = result.rows[0]
+
+      conn.release()
+      return product
+    } catch (err) {
+      throw new Error(`unable show product with id<${id}>: ${err}`)
+    }
+  }
+
+  create = async (name: string, price: number): Promise<Product> => {
+    try {
+      const conn = await client.connect()
+      const sql =
+        'INSERT INTO products (name, price) VALUES($1, $2) RETURNING *'
+
+      const result = await conn.query(sql, [name, price])
+      const product = result.rows[0]
+
+      conn.release()
+      return product
+    } catch (err) {
+      throw new Error(
+        `unable create product with name<${name}> price<${price}>: ${err}`,
+      )
+    }
+  }
+
+  delete = async (id: string): Promise<number> => {
+    try {
+      const conn = await client.connect()
+      const sql = 'DELETE FROM products where id=($1)'
+
+      const result = await conn.query(sql, [id])
+      const deletedRowCount = result.rowCount
+
+      conn.release()
+      return deletedRowCount
+    } catch (err) {
+      throw new Error(`unable delete product with id<${id}>: ${err}`)
+    }
+  }
+}
